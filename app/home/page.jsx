df23e2902bfe962108da77523716be12fc3c5740
rizkiramadhan <rizkiramadhanw234@gmail.com>
2025-12-11 20:35:34 +0700

    update content

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -178,15 +178,17 @@ export default function Home() {
           </h1>
 
           {/* Carousel Section */}
-          <div className="flex flex-col items-center justify-center mt-6 sm:mt-8 md:mt-10 px-4 sm:px-6">
+          <div
+            className="flex flex-col items-center justify-center mt-6 sm:mt-8 md:mt-10 px-4 sm:px-6"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
             <Carousel
               setApi={setWebApi}
               opts={{
                 align: "start",
               }}
               className="w-full max-w-4xl lg:max-w-6xl xl:max-w-7xl"
-              data-aos="fade-up"
-              data-aos-delay="100"
             >
               <CarouselContent className="-ml-2 md:-ml-4">
                 {RecentProjects.map((data, id) => (
