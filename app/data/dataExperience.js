2418139214d1e907cda263ccca52afa0c4bb00a0
rizkiramadhanw234-bit <rizkiramadhanw234@gmail.com>
2026-05-10 17:21:29 +0700

    update

--- app/data/dataExperience.js
+++ app/data/dataExperience.js
@@ -39,7 +39,7 @@ export const dataExperience = [
     ],
   },
   {
-    h1: "Kudukuats Project –  Kuningan, Indonesia",
+    h1: "Kudukuats Project –  Personal Freelance Brand",
     h2: "Full-Stack Web Developer – Freelance | Feb 2023 – March 2025",
     list: [
       "Built a School Registration Information System for a local institution.",
