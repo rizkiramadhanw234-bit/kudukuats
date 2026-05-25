722e6af32b83718453b4584faba976f7aab29f8e
rizkiramadhanw234-bit <rizkiramadhanw234@gmail.com>
2026-05-25 11:37:30 +0700

    update navbar

--- app/components/navbar/page.jsx
+++ app/components/navbar/page.jsx
@@ -11,6 +11,25 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = React.useState(false);
   const pathname = usePathname();
 
+  const NavItems = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Experience",
+      href: "/experience",
+    },
+    {
+      name: "Projects",
+      href: "/projects",
+    },
+    {
+      name: "Contact",
+      href: "/contact",
+    },
+  ]
+
   return (
     <nav className="fixed right-0 top-0 left-0 z-50 bg-(--bg2)/60 backdrop-blur-sm">
       <div className="px-4 sm:px-6 lg:px-10">
@@ -28,38 +47,17 @@ export default function Navbar() {
 
           {/* Desktop menu */}
           <div className="hidden md:flex items-center text-white text-base lg:text-base gap-6 lg:gap-10">
-            <Link
-              href={"/"}
-              className={`${
-                pathname === "/" ? "text-cyan-400" : "text-white"
-              } hover:scale-105 transition duration-300 px-1`}
-            >
-              Home
-            </Link>
-            <Link
-              href={"/experience"}
-              className={`${
-                pathname === "/experience" ? "text-cyan-400" : "text-white"
-              } hover:scale-105 transition duration-300 px-1`}
-            >
-              Experience
-            </Link>
-            <Link
-              href={"/projects"}
-              className={`${
-                pathname === "/projects" ? "text-cyan-400" : "text-white"
-              } hover:scale-105 transition duration-300 px-1`}
-            >
-              Projects
-            </Link>
-            <Link
-              href={"/contact"}
-              className={`${
-                pathname === "/contact" ? "text-cyan-400" : "text-white"
-              } hover:scale-110 transition duration-300 px-1`}
-            >
-              Contact
-            </Link>
+            {NavItems.map((item, index) => (
+              <Link
+                key={index}
+                href={item.href}
+                className={`${pathname === item.href ? "text-cyan-400" : "text-white"
+                  } hover:scale-105 transition duration-300`}
+              >
+                {item.name}
+              </Link>
+            ))}
+
           </div>
 
           <div className="hidden md:block hover:scale-105 transition duration-300">
@@ -96,45 +94,40 @@ export default function Navbar() {
 
       {/* Mobile menu open */}
       <div
-        className={`md:hidden w-full bg-transparent text-white flex flex-col transform transition-all duration-300 ease-in-out ${
-          isOpen
-            ? "opacity-100 max-h-screen translate-y-0"
-            : "opacity-0 max-h-0 -translate-y-5 pointer-events-none overflow-hidden"
-        }`}
+        className={`md:hidden w-full bg-transparent text-white flex flex-col transform transition-all duration-300 ease-in-out ${isOpen
+          ? "opacity-100 max-h-screen translate-y-0"
+          : "opacity-0 max-h-0 -translate-y-5 pointer-events-none overflow-hidden"
+          }`}
       >
         <div className="flex flex-col gap-1 p-4">
           <Link
             href={"/"}
-            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${
-              pathname === "/" ? "text-cyan-400 bg-gray-800/50" : ""
-            }`}
+            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${pathname === "/" ? "text-cyan-400 bg-gray-800/50" : ""
+              }`}
             onClick={() => setIsOpen(false)}
           >
             Home
           </Link>
           <Link
             href={"/experience"}
-            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${
-              pathname === "/experience" ? "text-cyan-400 bg-gray-800/50" : ""
-            }`}
+            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${pathname === "/experience" ? "text-cyan-400 bg-gray-800/50" : ""
+              }`}
             onClick={() => setIsOpen(false)}
           >
             Experience
           </Link>
           <Link
             href={"/projects"}
-            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${
-              pathname === "/projects" ? "text-cyan-400 bg-gray-800/50" : ""
-            }`}
+            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${pathname === "/projects" ? "text-cyan-400 bg-gray-800/50" : ""
+              }`}
             onClick={() => setIsOpen(false)}
           >
             Projects
           </Link>
           <Link
             href={"/contact"}
-            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${
-              pathname === "/contact" ? "text-cyan-400 bg-gray-800/50" : ""
-            }`}
+            className={`py-3 px-4 rounded-md hover:bg-gray-800 transition ${pathname === "/contact" ? "text-cyan-400 bg-gray-800/50" : ""
+              }`}
             onClick={() => setIsOpen(false)}
           >
             Contact
